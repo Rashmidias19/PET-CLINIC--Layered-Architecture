11aeefa2f5165d41996219ad6a6f646f84ed7520
Rashmidias19 <rashmidias819@gmail.com>
2023-03-16 21:49:36 +0530

    user login created alert added

--- src/main/java/Controllers/LoginFormController.java
+++ src/main/java/Controllers/LoginFormController.java
@@ -3,6 +3,7 @@ package Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -27,6 +28,8 @@ public class LoginFormController {
             stage.setTitle("Item Form");
             stage.centerOnScreen();
             stage.show();
+        }else{
+            new Alert(Alert.AlertType.ERROR, "Not a existing user!").show();
         }
     }
 
