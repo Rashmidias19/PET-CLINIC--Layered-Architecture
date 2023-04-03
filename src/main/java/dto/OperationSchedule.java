07b12f54ece22e4f764e1d07a31853db23a56b42
Rashmidias19 <rashmidias819@gmail.com>
2023-04-03 15:10:53 +0530

    new feature added-4

--- src/main/java/dto/OperationSchedule.java
+++ src/main/java/dto/OperationSchedule.java
@@ -1,4 +1,20 @@
 package dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+
 public class OperationSchedule {
+    private String OperationID;
+    private String PetID;
+    private String Date;
+    private String Time;
+    private String WorkTime;
+    private String Shift;
+    private String OT;
 }
