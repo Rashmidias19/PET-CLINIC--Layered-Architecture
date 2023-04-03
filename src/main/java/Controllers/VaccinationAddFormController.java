309ef6ad78c2e5ba5c19cb3b40f239d2e81a030f
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-04-03 18:26:43 +0530

    new one

--- src/main/java/Controllers/VaccinationAddFormController.java
+++ src/main/java/Controllers/VaccinationAddFormController.java
@@ -1,98 +1,143 @@
 package Controllers;
 
+import com.jfoenix.controls.JFXComboBox;
+import dto.Pet;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.EmployeeModel;
+import model.PetModel;
 
 import java.io.IOException;
-import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
+;
 
 
 
-public class VaccinationAddFormController implements Initializable{
-        public AnchorPane dashboardPane;
-        private static final String URL = "jdbc:mysql://localhost:3306/VETCLOUD";
-        private static final Properties props = new Properties();
 
-        static {
-            props.setProperty("user", "root");
-            props.setProperty("password", "1234");
-        }
-        public void petbtnOnAction(ActionEvent event) throws IOException {
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/PetManagementForm.fxml"))));
-            stage.setTitle("Item Form");
-            stage.centerOnScreen();
-            stage.show();
-        }
+public class VaccinationAddFormController implements Initializable {
+    public AnchorPane dashboardPane;
+    private static final String URL = "jdbc:mysql://localhost:3306/VETCLOUD";
+    private static final Properties props = new Properties();
 
-        public void customerbtnOnAction(ActionEvent event) throws IOException {
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/CustomerManagementForm.fxml"))));
-            stage.setTitle("Item Form");
-            stage.centerOnScreen();
-            stage.show();
-        }
+    static {
+        props.setProperty("user", "root");
+        props.setProperty("password", "1234");
+    }
 
-        public void usersbtnOnAction(ActionEvent event) throws IOException {
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/UserManagementForm.fxml"))));
-            stage.setTitle("Item Form");
-            stage.centerOnScreen();
-            stage.show();
-        }
+    @FXML
+    private JFXComboBox cmbPet_ID;
 
-        public void employeebtnOnAction(ActionEvent event) throws IOException {
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/EmployeeManagementForm.fxml"))));
-            stage.setTitle("Item Form");
-            stage.centerOnScreen();
-            stage.show();
-        }
 
-        public void suppliesbtnOnAction(ActionEvent event) throws IOException {
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/SupplieManagementForm.fxml"))));
-            stage.setTitle("Item Form");
-            stage.centerOnScreen();
-            stage.show();
-        }
+    public void petbtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/PetManagementForm.fxml"))));
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
 
-        public void billingbtnOnAction(ActionEvent event) throws IOException {
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/BillingManagementForm.fxml"))));
-            stage.setTitle("Item Form");
-            stage.centerOnScreen();
-            stage.show();
-        }
+    public void customerbtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/CustomerManagementForm.fxml"))));
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
 
-        public void inhousebtnOnAction(ActionEvent event) throws IOException {
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/InhouseManagementForm.fxml"))));
-            stage.setTitle("Item Form");
-            stage.centerOnScreen();
-            stage.show();
-        }
+    public void usersbtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/UserManagementForm.fxml"))));
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
 
-        public void logoutbtnOnAction(ActionEvent event) throws IOException {
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
-            stage.setTitle("Item Form");
-            stage.centerOnScreen();
-            stage.show();
-        }
+    public void employeebtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/EmployeeManagementForm.fxml"))));
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public void suppliesbtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/SupplieManagementForm.fxml"))));
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public void billingbtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/BillingManagementForm.fxml"))));
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public void inhousebtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/InhouseManagementForm.fxml"))));
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public void logoutbtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
 
     @Override
     public void initialize(java.net.URL url, ResourceBundle resourceBundle) {
+        loadPet_ID();
+    }
 
+    private void loadPet_ID() {
+        try {
+            ObservableList<String> obList = FXCollections.observableArrayList();
+            List<String> codes = EmployeeModel.loadEmployeeID();
 
+            for (String code : codes) {
+                obList.add(code);
+            }
+            cmbPet_ID.setItems(obList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "SQL Error!").show();
+        }
+    }
+
+    public void cmbIDOnAction(ActionEvent actionEvent) throws IOException {
+        String ID = (String) cmbPet_ID.getValue();
+        try {
+            Pet pet = PetModel.searchById(ID);
+            cmbPet_ID(pet);
+
+            // txtQty.requestFocus();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "SQL Error!").show();
+        }
+    }
+
+    private void cmbPet_ID(Pet pet) {
     }
 }
 
